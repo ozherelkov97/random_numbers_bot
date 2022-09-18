3793096317532c5d109e8af0933744254fae339b
ozherelkov97 <ozherelkov97@gmail.com>
2022-09-18 15:46:54 +0300

    it might be working now

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # random_numbers_bot
+This is a telegram bot that trains you to spell numbers in English.
+***
+Run **main.py** to start *infinity_polling()* of the bot on telegram server
+
+Bot token is .gitignored, ask *@ozherelkov* or create your own one in *@BotFather*
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 import random
+from typing import Union
+
 from num2words import num2words
+from telebot.types import Message
+from src.create_bot import bot, menu_keyboard, answer_keyboard
 
-small = random.randint(0, 99)
-good = random.randint(0, 9999)
-big = random.randint(99, 999999999)
-real = random.randint(0, 9999) + round(random.random(), 2)
 
-print(small, num2words(small))
-print(good, num2words(good) if num2words(good, to='cardinal') == num2words(good, to='year') else
-      f"{good}, {num2words(good, to='year')} ({num2words(good)})")
-print(big, num2words(big))
-print(real, num2words(real))
+@bot.message_handler(commands=['start'])
+def start_message(message: Message):
+    msg = bot.send_message(message.chat.id, 'Приятной тренировки! <3', reply_markup=menu_keyboard)
+    bot.register_next_step_handler(msg, menu)
 
+
+@bot.message_handler(content_types=['text'])
+def menu(message: Message):
+    if message.text == 'Маленькое':
+        num = random.randint(0, 99)
+        action = bot.send_message(message.chat.id, f'{num}', reply_markup=answer_keyboard)
+        bot.register_next_step_handler(action, cardinal, num)
+    elif message.text == 'Обычное':
+        num = random.randint(0, 9999)
+        action = bot.send_message(message.chat.id, f'{num}', reply_markup=answer_keyboard)
+        bot.register_next_step_handler(action, noncardinal, num)
+    elif message.text == 'Большое':
+        num = random.randint(99, 999999999)
+        action = bot.send_message(message.chat.id, f'{num}', reply_markup=answer_keyboard)
+        bot.register_next_step_handler(action, cardinal, num)
+    elif message.text == 'Нецелое':
+        num = random.randint(0, 9999) + round(random.random(), 2)
+        action = bot.send_message(message.chat.id, f'{num}', reply_markup=answer_keyboard)
+        bot.register_next_step_handler(action, cardinal, num)
+
+
+@bot.message_handler(content_types=['text'])
+def cardinal(message: Message, num: Union[int, float]):
+    reply = num2words(num)
+    action = bot.send_message(message.chat.id, f'{reply}', reply_markup=menu_keyboard)
+    bot.register_next_step_handler(action, menu)
+
+
+@bot.message_handler(content_types=['text'])
+def noncardinal(message: Message, num: Union[int, float]):
+    if num2words(num, to='cardinal') == num2words(num, to='year'):
+        reply = num2words(num)
+    else:
+        reply = f"{num2words(num, to='year')}\n" \
+                f"({num2words(num)})"
+    action = bot.send_message(message.chat.id, f'{reply}', reply_markup=menu_keyboard)
+    bot.register_next_step_handler(action, menu)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
